bbee1fdc250f8812b8115dc7cb4e0385db53bf08
celrati <m.charif.achraf@gmail.com>
2020-06-07 17:26:18 +0200

    styling and creating components

---
+++ boilerPlate/src/screens/ComponentsScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+const ComponentsScreen = () => {
+    const greeting = 'Hi there';
+    const greeting2 = <Text> Hi 2</Text>;
+    return <View>
+                <Text style={styles.textStyle}> This is components screen !</Text>
+                <Text style={styles.subHeaderStyle}>{greeting}</Text>
+                {greeting2}
+            </View>;
+};
+
+const styles = StyleSheet.create({
+    textStyle: {
+        fontSize : 30
+    },
+    subHeaderStyle: {
+        fontSize: 20
+    }
+});
+
+export default ComponentsScreen;
